496c7788860ca8be578e5f93cee2c2dbf54c179f
Eduardo Teles <edu.teles@pm.me>
2025-07-17 09:57:47 -0300

    Adiciona logger com pino e configura nível de log
    Implementa um sistema de logging usando a biblioteca pino com nível de log configurável via variável de ambiente LOG_LEVEL. Atualiza o script de desenvolvimento para usar pino-pretty para logs formatados e adiciona uma mensagem de inicialização no app worker.

--- apps/worker/src/env.ts
+++ apps/worker/src/env.ts
@@ -3,11 +3,15 @@ import {z} from "zod";
 
 export const env = createEnv({
   server: {
+    LOG_LEVEL: z.enum(["fatal", "error", "warn", "info", "debug", "trace", 'silent']).default("info"),
+
     REDIS_HOST: z.string().default("localhost"),
     REDIS_PORT: z.coerce.number().default(6379),
     REDIS_PASSWORD: z.string().optional()
   },
   runtimeEnv: {
+    LOG_LEVEL: process.env.LOG_LEVEL,
+
     REDIS_HOST: process.env.REDIS_HOST,
     REDIS_PORT: process.env.REDIS_PORT,
     REDIS_PASSWORD: process.env.REDIS_PASSWORD,
--- apps/worker/src/main.ts
+++ apps/worker/src/main.ts
@@ -1,6 +1,8 @@
 import {redis} from "@chatwoot-admin/worker/redis";
+import {logger} from "@chatwoot-admin/worker/logger";
 
 async function main() {
+  logger.info("Inicializando o app")
   await redis.set("hello", "world")
 }
 
