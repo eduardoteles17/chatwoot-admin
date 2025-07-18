3c55e79fc1258a893de70fef4183b02989bafcc6
Eduardo Teles <edu.teles@pm.me>
2025-07-18 10:04:46 -0300

    Utiliza esquema 'app' no schema do banco
    Altera a definição da tabela 'users' para usar o schema 'app', organizando melhor as tabelas no banco de dados e facilitando a manutenção futura do projeto.

--- packages/db/src/schema.ts
+++ packages/db/src/schema.ts
@@ -1,7 +1,9 @@
 import {v7 as uuidV7} from "uuid"
-import {uuid, pgTable, varchar, boolean, timestamp} from "drizzle-orm/pg-core";
+import {uuid, varchar, boolean, timestamp, pgSchema} from "drizzle-orm/pg-core";
 
-export const users = pgTable("users", {
+const appSchema = pgSchema("app")
+
+export const users = appSchema.table("users", {
   id: uuid("id").primaryKey().$defaultFn(() => uuidV7()),
   name: varchar({length: 255}).notNull(),
   email: varchar({length: 255}).notNull().unique(),
