f3ce988719c9796dacd981c293ec8578b2d8e8ea
Eduardo Teles <edu.teles@pm.me>
2025-07-29 08:57:11 -0300

    Remove schema do banco e integrações relacionadas
    Remove o arquivo de schema do banco de dados e elimina as importações e usos relacionados a ele nos pacotes e na aplicação worker. Também atualiza os arquivos package.json removendo dependências associadas ao banco e limpa o código para refletir essa exclusão.

--- apps/worker/src/db.ts
+++ apps/worker/src/db.ts
@@ -1,4 +1,1 @@
 import { env } from "@chatwoot-admin/worker/env";
-import { drizzle } from "drizzle-orm/node-postgres";
-
-export const db = drizzle(env.DATABASE_URL);
--- apps/worker/src/main.ts
+++ apps/worker/src/main.ts
@@ -1,6 +1,4 @@
-import { users } from "@chatwoot-admin/db";
 import { tryCatch } from "@chatwoot-admin/utils";
-import { db } from "@chatwoot-admin/worker/db";
 import { logger } from "@chatwoot-admin/worker/logger";
 import { redis } from "@chatwoot-admin/worker/redis";
 
@@ -19,15 +17,6 @@ async function main() {
   logger.info("Inicializando o app");
 
   await initRedis();
-
-  const user: typeof users.$inferInsert = {
-    name: "TEste",
-    email: "",
-  };
-
-  await db.insert(users).values(user);
-  const usersRe = await db.select().from(users);
-  console.log("users", usersRe);
 }
 
 main();
