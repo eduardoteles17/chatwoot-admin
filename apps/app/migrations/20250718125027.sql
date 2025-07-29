cfe3e6e550e55c4b33e5c0216917a137bedae681
Eduardo Teles <edu.teles@pm.me>
2025-07-29 08:18:24 -0300

    Remover migrações e configuração do banco da aplicação
    Excluir arquivos de migrações SQL, esquema Atlas e configuração do banco PostgreSQL relacionados ao schema "app". Essa remoção limpa o histórico de alterações do banco e suas configurações associadas.

--- apps/app/migrations/20250718125027.sql
+++
@@ -1,6 +0,0 @@
--- Drop schema named "public"
-DROP SCHEMA "public" CASCADE;
--- Add new schema named "app"
-CREATE SCHEMA "app";
--- Create "users" table
-CREATE TABLE "app"."users" ("id" uuid NOT NULL, "name" character varying(255) NOT NULL, "email" character varying(255) NOT NULL, "emailVerified" boolean NOT NULL DEFAULT false, "updatedAt" timestamp NOT NULL, PRIMARY KEY ("id"), CONSTRAINT "users_email_unique" UNIQUE ("email"));
