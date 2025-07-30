fd2b8bd69232f398c39a83df1fdc2177a3a5f161
Eduardo Teles <edu.teles@pm.me>
2025-07-30 09:49:47 -0300

    Adiciona integração OAuth com NextAuth e configura variáveis de ambiente
    Implementa provedor OAuth usando OIDC no NextAuth com suporte a PKCE e verificação de estado para autenticação segura. Atualiza validação das variáveis de ambiente para incluir configurações necessárias ao OAuth.

--- apps/app/src/auth.ts
+++ apps/app/src/auth.ts
@@ -6,9 +6,37 @@ import NextAuth from "next-auth";
 export const { handlers, signIn, signOut, auth } = NextAuth({
   secret: env.AUTH_SECRET,
   adapter: PrismaAdapter(db),
+  session: {
+    strategy: "jwt"
+  },
   pages: {
     signIn: "/auth/login",
     signOut: "auth/login",
   },
-  providers: [],
+  providers: [
+    {
+      id: "oauth",
+      name: "OAuth",
+      type: "oidc",
+      authorization: {
+        params: {
+          scope: env.OAUTH_SCOPE,
+        },
+      },
+      clientId: env.OAUTH_CLIENT_ID,
+      clientSecret: env.OAUTH_CLIENT_SECRET,
+      wellKnown: env.OAUTH_WELL_KNOWN_URL,
+      issuer: env.OAUTH_ISSUER,
+      checks: ["pkce", "state"],
+      idToken: true,
+      async profile(profile) {
+        return {
+          id: profile.sub,
+          name: profile.name,
+          email: profile.email,
+          image: profile.picture,
+        };
+      },
+    },
+  ],
 });
--- apps/app/src/env.ts
+++ apps/app/src/env.ts
@@ -6,11 +6,37 @@ export const env = createEnv({
     AUTH_SECRET: z.string(),
 
     DATABASE_URL: z.url(),
+
+    OAUTH_SCOPE: z.string().min(1).optional().default("openid profile email"),
+    OAUTH_CLIENT_ID: z.string().min(1),
+    OAUTH_CLIENT_SECRET: z.string().min(1),
+    OAUTH_WELL_KNOWN_URL: z.url(),
+    OAUTH_ISSUER: z.preprocess((val) => {
+      if (!val) {
+        return process.env.OAUTH_WELL_KNOWN_URL?.split("/.well-known")[0];
+      }
+
+      return val;
+    }, z.url().optional()),
+    OAUTH_USER_ID_PATH: z.string().min(1).optional().default("id"),
+    OAUTH_USER_NAME_PATH: z.string().min(1).optional().default("name"),
+    OAUTH_USER_EMAIL_PATH: z.string().min(1).optional().default("email"),
+    OAUTH_USER_IMAGE_PATH: z.string().min(1).optional().default("image"),
   },
   client: {},
   runtimeEnv: {
     AUTH_SECRET: process.env.NODE_ENV,
 
     DATABASE_URL: process.env.DATABASE_URL,
+
+    OAUTH_SCOPE: process.env.OAUTH_SCOPE,
+    OAUTH_CLIENT_ID: process.env.OAUTH_CLIENT_ID,
+    OAUTH_CLIENT_SECRET: process.env.OAUTH_CLIENT_SECRET,
+    OAUTH_WELL_KNOWN_URL: process.env.OAUTH_WELL_KNOWN_URL,
+    OAUTH_ISSUER: process.env.OAUTH_ISSUER,
+    OAUTH_USER_ID_PATH: process.env.OAUTH_USER_ID_PATH,
+    OAUTH_USER_NAME_PATH: process.env.OAUTH_USER_NAME_PATH,
+    OAUTH_USER_EMAIL_PATH: process.env.OAUTH_USER_EMAIL_PATH,
+    OAUTH_USER_IMAGE_PATH: process.env.OAUTH_USER_IMAGE_PATH,
   },
 });
